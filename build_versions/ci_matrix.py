91c107c6cbdf259bf5c0ebdced8001ef007a60a0
Nikolai R Kristiansen <nikolai.kristiansen@remarkable.no>
2023-02-07 21:27:37 +0100

    refactor: use environment files

--- build_versions/ci_matrix.py
+++ build_versions/ci_matrix.py
@@ -1,10 +1,16 @@
 import json
+import os
+from pathlib import Path
 
 CI_EVENT_SCHEDULED = "scheduled"
 
 
-def job_with_name(job, name):
-    return isinstance(job, dict) and name in job
+def _github_action_set_output(key: str, value: str):
+    """Write
+    https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
+    """
+    with Path(os.getenv("GITHUB_OUTPUT")).open("a") as fp:
+        fp.write(f"{key}={value}")
 
 
 def generate_matrix(new_or_updated: list, ci_event: str):
@@ -13,7 +19,6 @@ def generate_matrix(new_or_updated: list, ci_event: str):
         print("\n# Scheduled run with no new or updated versions. Doing nothing.")
         return
 
-    # FIXME: https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-environment-variable
-    print(f"::set-output name=matrix::{json.dumps({'include': new_or_updated})}")
+    _github_action_set_output("MATRIX", json.dumps({"include": new_or_updated}))
     print("\n# New or updated versions:")
     print("Nothing" if not new_or_updated else "\n".join(version["key"] for version in new_or_updated))
